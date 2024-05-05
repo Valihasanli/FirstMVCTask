542101bf2146e79831afcfe8cba6c7dbc2cc16c0
Veli Hesenli <BD7RGSR0D@code.edu.az>
2024-05-05 23:43:40 +0400

    Ne yazdim Nece yazdim mene de maraqlidi

---
+++ FurnitureShop/Controllers/FurnitureController.cs
@@ -0,0 +1,43 @@
+﻿using FurnitureShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShop.Controllers
+{
+    public class FurnitureController : Controller
+    {
+        private List<Furniture> CreateFurnitureList()
+        {
+            var furnitureList = new List<Furniture>();
+            Furniture furniture1 = new Furniture
+            {
+                Name = "Divan",
+                Id = 1,
+                Price=123
+            };
+            Furniture furniture2 = new Furniture
+            {
+                Name = "Stol",
+                Id = 2,
+                Price = 423
+            };
+            Furniture furniture3 = new Furniture
+            {
+                Name = "Stul",
+                Id = 3,
+                Price = 50
+            };
+            furnitureList.Add(furniture1);
+            furnitureList.Add (furniture2);
+            furnitureList.Add(furniture3);
+            return furnitureList;
+        }
+        public ActionResult OurDesign()
+        {
+            var furnitureList = CreateFurnitureList();
+
+            ViewBag.FurnitureList = furnitureList;
+
+            return View();
+        }
+    }
+}
---
+++ FurnitureShop/Models/Furniture.cs
@@ -0,0 +1,9 @@
+﻿namespace FurnitureShop.Models
+{
+    public class Furniture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
